8a7d5efa357e41e5de65ffd7ca3ea0f6e83375a4
Ricardo Gouveia <ricardogouveia3_@hotmail.com>
2024-04-26 07:38:48 -0300

    Add tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className="text-center">
+      <header className="bg-[#282c34] min-h-[100vh] flex text-white flex-col items-center justify-center text-[calc(10px + 2vmin)]">
+        <img
+          src={logo}
+          className="pointer-events-none h-[40vmin] animate-[spin_20s_linear_infinite]"
+          alt="logo"
+        />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <a
-          className="App-link"
+          className="text-[#61dafb]"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
