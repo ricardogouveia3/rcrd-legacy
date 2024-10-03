b06e502ba8420a47b246b0a5747778d63d141df0
Ricardo Gouveia <ricardogouveia3_@hotmail.com>
2024-10-03 17:39:48 -0300

    redesign ready

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,26 @@
 import { useState } from "react";
 
 function App() {
-  const [selectedProject, setSelectedProject] = useState("");
+  const [selectedProject, setSelectedProject] = useState(
+    "/projects/empty/index.html"
+  );
 
   const handleChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
     setSelectedProject(event.target.value);
   };
 
   return (
-    <div>
-      <header>
-        <h1>Exibir Projetos Antigos</h1>
-        <label htmlFor="project-select">Selecione o Projeto:</label>
-        <select id="project-select" onChange={handleChange}>
-          <option value="">-- Selecione --</option>
+    <main className="flex flex-col h-screen">
+      <header className="w-full bg-[#4a4a4a] p-4 border-b-2 border-white text-white flex justify-center gap-8 align-middle items-center">
+        <h1 className="font-semibold">RCRD Legacy Projects</h1>
+        <select
+          id="project-select"
+          onChange={handleChange}
+          className="bg-white text-black focus:outline-none px-4 py-2"
+        >
+          <option value="" disabled selected>
+            -- select a project --
+          </option>
           <option value="projects/site-v1/index.html">Site V1</option>
           <option value="projects/site-v2/index.html">Site V2</option>
           <option value="projects/site-v3/index.html">Site V3</option>
@@ -21,16 +28,17 @@ function App() {
         </select>
       </header>
 
-      {selectedProject && (
+      <div className="flex-grow">
         <iframe
           src={selectedProject}
           title="Projeto"
           sandbox="allow-same-origin allow-scripts"
-          style={{ width: "100%", height: "80vh", border: "none" }}
-          loading="lazy"
+          style={{ width: "100%", height: "100%", border: "none" }}
+          loading="eager"
+          className="iframeresponsive-iframe"
         />
-      )}
-    </div>
+      </div>
+    </main>
   );
 }
 
